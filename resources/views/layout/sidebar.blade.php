40e3df545659c90f862a16bf65b5d701d9271c3a
jrlaravel <jrlaravel.digieagleinc@gmail.com>
2024-10-30 14:03:23 +0530

    local changes

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <title>Dashboard Physiofiness</title>
     <!-- CSS files -->
     <link href="{{asset('css/tabler.min.css?1692870487')}}" rel="stylesheet"/>
     <link href="{{asset('css/tabler-flags.min.css?1692870487')}}" rel="stylesheet"/>
